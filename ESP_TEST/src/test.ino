5f96d09123d4583342bc204ae5b0cf44532857fe
kristsm <kristsm@gmail.com>
2016-02-26 16:32:07 +0200

    Update test.ino

--- ESP_TEST/src/test.ino
+++ ESP_TEST/src/test.ino
@@ -3,6 +3,9 @@
 #include <WiFiClient.h>
 #include <WebSocketsServer.h>
 #include <Hash.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 WebSocketsServer webSocket = WebSocketsServer(81);
 const char *ssid = "ESP";
 const char *password = "12345678";
@@ -58,16 +61,48 @@ void setup() {
 
     Serial.begin(115200);
     pinMode(16,OUTPUT);
+    WiFi.softAP(ssid, password);
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
 
-   WiFi.softAP(ssid, password);
+    
+    // OTA STARTS
+        // Port defaults to 8266
+        // ArduinoOTA.setPort(8266);
 
- IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+        // Hostname defaults to esp8266-[ChipID]
+        // ArduinoOTA.setHostname("myesp8266");
+
+        // No authentication by default
+        // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+    // OTA ENDS
+    
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
+    
 }
 
 void loop() {
+    ArduinoOTA.handle();
     webSocket.loop();
 }
